aeb7d30d085373f4c5705fdf8446fa99745c1a9d
Christina <christina.golovach@gmail.com>
2018-12-11 18:45:32 +0300

    Add ORM project

---
+++ DAL/Repositories/AccountRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DAL.Interface.DTO;
+using DAL.Interface.Interfaces;
+using ORMDBFirst;
+
+namespace DAL.Repositories
+{
+    public class AccountRepository : IAccountRepository
+    {
+        private readonly DbContext dbContext;
+
+        public AccountRepository(DbContext context)
+        {
+            dbContext = context;
+        }
+
+        public void Add(AccountDTO account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<AccountDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public AccountDTO GetByNumber(string number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(AccountDTO account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DAL/Repositories/OwnerRepository.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DAL.Interface.DTO;
+using DAL.Interface.Interfaces;
+using ORMDBFirst;
+
+namespace DAL.Repositories
+{
+    public class OwnerRepository : IOwnerRepository
+    {
+        private readonly DbContext dbContext;
+
+        public OwnerRepository(DbContext context)
+        {
+            dbContext = context;
+        }
+
+        public void Add(OwnerDTO owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<OwnerDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public OwnerDTO GetByPassportNumber(string passportNumber)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(OwnerDTO owner)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
