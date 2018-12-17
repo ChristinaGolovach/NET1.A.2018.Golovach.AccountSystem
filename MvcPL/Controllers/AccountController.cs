293f38bdda4ea65be067707005134113df8fa8c2
Christina <christina.golovach@gmail.com>
2018-12-17 23:45:17 +0300

    Add MvcPL with the correct version of Ninject

---
+++ MvcPL/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BLL.Interface.Interfaces;
+
+namespace MvcPL.Controllers
+{
+    public class AccountController : Controller
+    {
+        IAccountService accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            this.accountService = accountService;
+        }
+
+        // GET: Account
+        public ActionResult Index()
+        {
+            var accounts = accountService.GetAllAccounts();
+
+            //for chek only
+            foreach(var account in accounts)
+            {
+
+            }
+            return View();
+        }
+
+    }
+}
---
+++ MvcPL/Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using Ninject;
+using Ninject.Web.Mvc;
+using DependencyResolver;
+
+namespace MvcPL
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            IKernel resolver = new StandardKernel();
+            resolver.ConfigurateResolverWeb();
+            System.Web.Mvc.DependencyResolver.SetResolver(new NinjectDependencyResolver(resolver));
+        }
+    }
+}
